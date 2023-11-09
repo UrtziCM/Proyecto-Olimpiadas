f6ce04636a6fd3ef2ed43835185c784285164860
Urtzi <urcrmedev@gmail.com>
2023-11-09 08:21:42 +0100

    ADD MainApplication class to run the application

---
+++ src/net/urtzi/olimpiadas/application/MainApplication.java
@@ -0,0 +1,30 @@
+package net.urtzi.olimpiadas.application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+	@Override
+    public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(this.getClass().getResource("/fxml/olimpiadas.fxml"));
+	    	Scene scene = new Scene( root );
+	    	
+	        stage.setTitle("Olimpiadas - Gestion");
+	        stage.setScene(scene);
+	        stage.setResizable(false);
+	        stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
